1cf38560e145dc0eb4630b0441e350afff36c243
Morrism1 <morrismontana0@gmail.com>
2023-06-06 18:08:37 +0200

    chore: refactor exercises

---
+++ exercise6.js
@@ -0,0 +1,5 @@
+function maxNumber (number1, number2, number3) {
+  return Math.max(number1, number2, number3)
+}
+
+export default maxNumber
---
+++ exercise7.js
@@ -0,0 +1,5 @@
+function multiplication (number1, number2) {
+  return number1 * number2
+}
+
+export default multiplication
