47b66f0bb49f798e5e0c0ae67240461395958887
Morrism1 <morrismontana0@gmail.com>
2023-06-08 21:58:46 +0200

    chore: expand the for loop

--- prime.js
+++ prime.js
@@ -1,6 +1,8 @@
 function isPrime (number_) {
   if (number_ < 2) return false
-  for (let index = 2, sqrtNumber = Math.sqrt(number_); index <= sqrtNumber; index++) { if (number_ % index === 0) return false }
+  for (let index = 2, sqrtNumber = Math.sqrt(number_); index <= sqrtNumber; index++) {
+    if (number_ % index === 0) return false
+  }
   return true
 }
 
